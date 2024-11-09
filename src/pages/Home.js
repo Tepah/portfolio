e8e89c48e858d77f94378650f7bff91a2f2688bc
Tepah <67202848+Tepah@users.noreply.github.com>
2024-11-08 16:26:40 -0800

    Playing around with Hero a bit, good starting base

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import PageTransition from "../components/PageTransition";
-import heroImage from "../assets/IMG_0697.jpg";
 
 const Home = () => {
   return (
     <PageTransition>
+      <BackgroundImage />
       <div>
         <h1 className="text-2xl">Home</h1>
         <div className="flex flex-col">
@@ -27,14 +27,24 @@ const Home = () => {
 
 const Super = () => {
   return (
-    <div>
-      <h1>
-        I am Pete
-      </h1>
-      <div className="w-32 h-32 overflow-hidden">
-        <img src={heroImage} className="object-cover w-full h-full" alt="Pete" />
+    <div className="w-screen h-screen backdrop-opacity-0 flex items-center justify-center">
+      <div className="-mt-96">
+        <h1 className="text-7xl">
+          I am Pete
+        </h1>
       </div>
     </div>
+  )
+}
+
+const BackgroundImage = () => {
+  const heroImage = require("../assets/IMG_0697.jpg");
+
+  return (
+    <div
+      className="fixed top-1/2 left-1/2 w-64 h-64 bg-cover bg-center transform -translate-x-1/2 -translate-y-1/2 z-[-1]"
+      style={{ backgroundImage: `url(${heroImage})` }}
+    />
   )
 }
 
