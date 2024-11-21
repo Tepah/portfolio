91ee1be47cb1a57bf567f13472656b593b36a3ec
Tepah <67202848+Tepah@users.noreply.github.com>
2024-11-20 23:31:04 -0800

    Starting to create layers for project presentation

--- src/pages/About.js
+++ src/pages/About.js
@@ -5,10 +5,51 @@ const About = () => {
   return (
     <PageTransition>
       <div>
-        <h1>About</h1>
-        <p></p>
+        <h1>Things about me</h1>
       </div>
     </PageTransition>
+  )
+}
+
+const aboutLayer = () => {
+  return (
+    <div className="flex flex-row">
+      <div>
+        <Image src="https://via.placeholder.com/150" alt="about" />
+      </div>
+      <div>
+        <h1>About me</h1>
+        <p>Here are some things about me</p>
+      </div>
+    </div>
+  )
+}
+
+const skillsLayer = () => {
+  return (
+    <div className="flex flex-row">
+      <div>
+        <Image src="https://via.placeholder.com/150" alt="skills" />
+      </div>
+      <div>
+        <h1>Skills</h1>
+        <p>Here are some of my skills</p>
+      </div>
+    </div>
+  )
+}
+
+const experienceLayer = () => {
+  return (
+    <div className="flex flex-row">
+      <div>
+        <Image src="https://via.placeholder.com/150" alt="experience" />
+      </div>
+      <div>
+        <h1>Experience</h1>
+        <p>Here are some of my experiences</p>
+      </div>
+    </div>
   )
 }
 
--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -6,10 +6,38 @@ const Contact = () => {
     <PageTransition>
       <div>
         <h1>Contact</h1>
-        <p>Contact me at these websites</p>
+        <p>Here are some ways to contact me</p>
 
       </div>
     </PageTransition>
+  )
+}
+
+const instagramLayer = () => {
+  return (
+    <div className="flex flex-row">
+      <div>
+        <Image src="https://via.placeholder.com/150" alt="instagram" />
+      </div>
+      <div>
+        <h1>Instagram</h1>
+        <p>Here is my Instagram</p>
+      </div>
+    </div>
+  )
+}
+
+const emailLayer = () => {
+  return (
+    <div className="flex flex-row">
+      <div>
+        <Image src="https://via.placeholder.com/150" alt="email" />
+      </div>
+      <div>
+        <h1>Email</h1>
+        <p>Here is my Email</p>
+      </div>
+    </div>
   )
 }
 
